cead0c0ce4dfbaf2296965d4524cb7fa35866951
running-mars <guanzhengcn@foxmail.com>
2020-12-30 11:14:46 +0800

    modify

--- OpenMP_Programming/prog_5.4_oddeven_sort.c
+++ OpenMP_Programming/prog_5.4_oddeven_sort.c
@@ -16,7 +16,7 @@ int main(int argc, char* argv[]){
     for (int phase = 0; phase < n; phase++){
         if (phase % 2 == 0)
 #           pragma omp parallel for num_threads(thread_count) \
-                default(none) shared(a, n) private(i, tnp)
+                default(none) shared(a, n) private(i, tmp)
             for (int i=1; i<n; i+=2){
                 if (a[i-1] > a[i]){
                     tmp = a[i-1];
@@ -26,7 +26,7 @@ int main(int argc, char* argv[]){
             }
             else
 #           pragma omp parallel for num_threads(thread_count) \
-                default(none) shared(a, n) private(i, tnp)
+                default(none) shared(a, n) private(i, tmp)
             for (int i=1; i<n-1; i+=2){
                 if (a[i] > a[i+1]){
                     tmp = a[i+1];
