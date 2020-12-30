eb38f518b22dca0308d0bc1bc5b9d984a257886c
running-mars <guanzhengcn@foxmail.com>
2020-12-30 20:20:06 +0800

    modify

--- OpenMP_Programming/schedule.c
+++ OpenMP_Programming/schedule.c
@@ -7,12 +7,12 @@ double f(int i);
 
 int main(int argc, char* argv[]){
     int thread_count = strtol(argv[1], NULL, 10);
-    int total_num = 900000;
+    int total_num = 500000;
     double sum = 0;
     double start, finish, elapsed;
     GET_TIME(start);
 #   pragma omp parallel for num_threads(thread_count) \
-        reduction(+:sum)
+        reduction(+:sum) schedule(runtime)
     for (int i=0; i<total_num; i++){
 		sum += f(i);
     }
@@ -30,10 +30,7 @@ double f(int i){
     double return_val = 0.0;
 
     for (j=start; j<=finish; j++){
-        if (j == 0){
-            j += 1;
-        }
-        return_val += j*j/j/j + 0.001;
+        return_val += j + 0.001;
     }
     return return_val;
 }
