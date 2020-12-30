c3419ba0ebf60931014e2751d9ccb9ae9d91929d
running-mars <guanzhengcn@foxmail.com>
2020-12-30 19:34:46 +0800

    add schedule.c

---
+++ OpenMP_Programming/schedule.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include "timer.h"
+
+double f(int i);
+
+int main(int argc, char* argv[]){
+    int thread_count = strtol(argv[1], NULL, 10);
+    long long int total_num = 900000;
+    long long int total_count = 0;
+    double start, finish, elapsed;
+    GET_TIME(start);
+#   pragma omp parallel for num_threads(thread_count) \
+        reduction(+:total_count)
+    for (long long int i=0; i<total_num; i++){
+		total_count += f(i);
+    }
+    GET_TIME(finish);
+    elapsed = finish - start;
+    printf("Time elapsed: %lf seconds. \n", elapsed);
+    return 0;
+}
+
+
+double f(int i){
+    int j;
+    int start = i*(i+1)/2;
+    int finish = start + i;
+    double return_val = 0.0;
+
+    for (j=start; j<=finish; j++){
+        return_val += j*j/j/j + 0.001;
+    }
+    return return_val;
+}
