de873ff0cba85074314782b19afe8a2134378054
running-mars <guanzhengcn@foxmail.com>
2020-12-30 15:19:11 +0800

    modify

--- OpenMP_Programming/prog_table5.2.c
+++ OpenMP_Programming/prog_table5.2.c
@@ -14,10 +14,11 @@ int main(int argc, char* argv[]){
     int i;
     double start, finish, elapsed;
     GET_TIME(start);
-    for (int phase = 0; phase < n; phase++){
+#   pragma omp parallel num_threads(thread_count) \
+        default(none) shared(a, n) private(i, tmp, phase)
+    for (phase = 0; phase < n; phase++){
         if (phase % 2 == 0)
-#           pragma omp parallel for num_threads(thread_count) \
-                default(none) shared(a, n) private(i, tmp)
+#           pragma omp for
             for (i=1; i<n; i+=2){
                 if (a[i-1] > a[i]){
                     tmp = a[i-1];
@@ -26,8 +27,7 @@ int main(int argc, char* argv[]){
                 }
             }
         else
-#           pragma omp parallel for num_threads(thread_count) \
-                default(none) shared(a, n) private(i, tmp)
+#           pragma omp for
             for (i=1; i<n-1; i+=2){
                 if (a[i] > a[i+1]){
                     tmp = a[i+1];
