7ac4eb568aa548c7eb7e17ba7115d27dbc4b0596
running-mars <guanzhengcn@foxmail.com>
2021-01-26 09:02:14 +0800

    modify

--- OpenMP_Programming/prog_5.1_hello_world.c
+++ OpenMP_Programming/prog_5.1_hello_world.c
@@ -8,13 +8,13 @@ int main(int argc, char* argv[]){
     int thread_count = strtol(argv[1], NULL, 10);
 
 #   pragma omp parallel num_threads(thread_count)
+    /* The following section is executed by all the thread */
     Hello();
 
     return 0;
 }
 
 void Hello(void){
-
     int my_rank = omp_get_thread_num();
     int thread_count = omp_get_num_threads();
 
